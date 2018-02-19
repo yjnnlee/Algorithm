f1d4680ab4c5320f6144387a465c2bab708070d9
Yujin Lee <proqk@naver.com>
2018-02-19 09:57:11 +0900

    Update 9938 방 청소.cpp

--- disjoint_set/9938 방 청소.cpp
+++ disjoint_set/9938 방 청소.cpp
@@ -2,60 +2,57 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int n, m, a,b,c;
+int n, m, a, b, c;
 int drawer[300005];
+bool res = false;
+int parent[300005];
 
 struct F {
-	vector<int> parent, rank;
-	F(int n) :parent(n), rank(n, 1) {
-		for (int i = 0; i < n; i++) {
+//	vector<int> parent, rank;
+	F(int n, int l){
+		for (int i = 1; i <= l; i++) {
 			parent[i] = i;
 		}
 	}
-	int len(int a) {
-		a = find(a);
-		return rank[a];
-	}
 
 	int find(int u) {
-		if (u == parent[u]) return u;
-		return parent[u] = find(parent[u]);
+		if (u == parent[u]) return u; //루트 노드면 리턴
+		return parent[u] = find(parent[u]); //아니면 루트를 찾으러
 	}
 	void merge(int u, int v) {
 		u = find(u);
 		v = find(v);
-		if (u == v) return; //같은 트리
-		if (rank[u] > rank[v]) swap(u, v);
-		if (rank[u] == rank[v]) rank[v]++; //같은 높이면 1더함
+		//if (u == v) return; //같은 트리면
+		//if (rank[u] > rank[v]) swap(u, v);
+		//if (rank[u] == rank[v]) rank[v]++; //같은 깊이면 깊이를 늘려준다
 		parent[u] = v; //자식으로 넣음
-
+		res = true;
+		printf("LADICA\n");
 	}
 };
 
 int main() {
 	scanf("%d%d", &n, &m);
-	F f(m * 2);
+	F f(300005, m*2);
 	for (int i = 0; i < n; i++) {
-		bool res = false;
 		scanf("%d%d", &a, &b);
-		if (f.find(a) == a) {
+		if (!drawer[a]) {
+			drawer[a] = true;
 			f.merge(a, b);
-			res = true;
 		}
-		else if(f.find(b) == b){
+		else if (!drawer[b]) {
+			drawer[b] = true;
+			f.merge(b, a);
+		}
+		else if (!drawer[(f.find(a))]) {
+			drawer[f.find(a)] = true;
 			f.merge(a, b);
-			res = true;
 		}
-		//f.merge(a, b);
-		//if (f.len(a) + f.len(b) - 1 < 0) {
-		//	printf("SMECE\n");
-		//}
-		//else {
-		//	f.len(a) += f.len(b) - 1;
-		//	printf("LADICA\n");
-		//}
+		else if (!drawer[(f.find(b))]) {
+			drawer[f.find(b)] = true;
+			f.merge(b, a);
+		}
 		if (res) printf("LADICA\n");
-		else printf("SMECE\n");
+		res = false;
 	}
 }
-
