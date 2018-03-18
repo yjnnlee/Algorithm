5145d429a418e98d312b073f6b9439d9e24b8779
Yujin Lee <proqk@naver.com>
2018-03-19 00:05:19 +0900

    Create 1012 유기농 배추.cpp

---
+++ dfs/1012 유기농 배추.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <memory.h>
+
+int map[55][55], t, a, b, k, cnt = 0, x, y;
+int w[4] = { -1,1,0,0 };
+int h[4] = { 0,0,-1,1 };
+
+void dfs(int x, int y) {
+	map[x][y] = 0;
+	for (int i = 0; i < 4; i++)	
+		if (map[x + w[i]][y + h[i]]) dfs(x + w[i], y + h[i]);
+
+	//		if (p < 0 || q < 0 || p >= x || q >= y) continue;
+	//	if (map[x - 1][y] || map[x + 1][y] || map[x][y - 1] || map[x][y + 1]) {}
+}
+
+int main() {
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		cnt = 0, memset(map, 0, sizeof(map));
+		scanf("%d%d%d", &x, &y, &k);
+		for (int j = 0; j < k; j++) {
+			scanf("%d%d", &a, &b);
+			map[++a][++b] = 1;
+		}
+
+		for (int j = 1; j <= x; j++)
+			for (int jj = 1; jj <= y; jj++)
+				if (map[j][jj]) dfs(j, jj), cnt++;
+
+		printf("%d\n", cnt);
+	}
+}
