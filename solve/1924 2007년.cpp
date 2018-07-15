7608b3b4da901470ff9bf6f51cafa2bb7ba07b92
Yujin Lee <proqk@naver.com>
2018-07-15 20:26:03 +0900

    Create 1924 2007년.cpp

---
+++ solve/1924 2007년.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int month[] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+char *day[] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+int a, b;
+int main(){
+	scanf("%d%d", &a, &b);
+	for (int i = 0; i < a - 1; i++) {
+		b += month[i];
+	}
+	printf("%s", day[b % 7]);
+}
