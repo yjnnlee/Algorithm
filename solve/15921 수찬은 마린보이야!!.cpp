002313d16efea1911b6266857d301d826c1e1f63
Yujin Lee <proqk@naver.com>
2018-07-20 23:36:27 +0900

    Create 15921 수찬은 마린보이야!!.cpp

---
+++ solve/15921 수찬은 마린보이야!!.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int n, a, sum = 0;
+int main() {
+	scanf("%d", &n);
+	if (n == 0) {
+		printf("divide by zero");
+		return 0;
+	}
+	while (n--) {
+		scanf("%d", &a);
+	}
+	printf("1.00");
+}
