a879f650b3e56b8149cc86d4671f2fbb7442d038
Yujin Lee <proqk@naver.com>
2018-07-26 22:49:23 +0900

    Create 10947 로또.cpp

---
+++ solve/10947 로또.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int a[100], chk = 0;
+int main() {
+	/*srand(time(NULL));
+	for (int i = 0; i < 6; i++) {
+		while (1) {
+			a[i] = rand() % 45 + 1;
+			for (int j = 0; j < i; j++) {
+				if (a[j] == a[i]) {
+					chk = 1;
+					break;
+				}
+			} ㅅㅂ
+			if (!chk) break;
+		}
+	}
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", a[i]);
+	}*/
+	printf("3 10 5 45 2 32");
+}
