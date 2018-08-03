054b74dd9828feeb45f9f35ac10ca6bf37a7aef2
Yujin Lee <proqk@naver.com>
2018-08-03 16:05:24 +0900

    Update BOARDCOVER.cpp

--- Algospot/BOARDCOVER.cpp
+++ Algospot/BOARDCOVER.cpp
@@ -8,12 +8,12 @@ int covertype[4][3][2] = { //시작지점, y, x
 	{ {0,0}, {1,0}, {1,-1}} //┘
 };
 bool possible(int y, int x, int type, int sw) { //놓을 수 있는가?
-	for (int i = 1; i <= 4; i++) {
-		int ny = y + covertype[type][i][0];
-		int nx = x + covertype[type][i][1];
-		if (ny < 0 || ny >= h || nx < 0 || nx >= w) return false;
-		if (sw == 1) map[ny][nx] = '#';
-		if (sw == 0) map[ny][nx] = '.';
+	for (int i = 0; i < 4; i++) {
+		int ny = y + covertype[type][i][0]; //1+0
+		int nx = x + covertype[type][i][1]; //0+0
+//		printf(">%d %d\n", ny, nx);
+		if (ny < 0 || ny >= h || nx < 0 || nx >= w) return false; //넘으면 안 된다
+		else if ((map[ny][nx] += sw) > 1) return false; //되돌려 놓는다
 	}
 	return true;
 }
@@ -26,19 +26,20 @@ int set() { //놓는다
 				break;
 			}
 		}
-//		if (y != -1) break; //y가 움직였다는건
+		if (y != -1) break; //y가 움직였다는건 더 볼 필요 없이 여기부터 탐색(?)
+		//그런 뜻인거 같긴 한데 종만북에 이 부분 이해가 잘 안 됨
 	}
 	if (y == -1) return 1; //모든 칸을 채웠으면 리턴
 	int cnt = 0;
 	for (int type = 0; type < 4; type++) {
 		if (possible(y, x, type, 1)) cnt += set();
-		possible(y, x, type, 0);
+		possible(y, x, type, -1);
 	}
 	return cnt;
 }
 
 int main() {
-	scanf("%d", &c);
+	scanf("%d", &c); 
 	while (c--) {
 		scanf("%d %d ", &h, &w);
 		for (int i = 0; i < h; i++) {
@@ -61,3 +62,4 @@ int main() {
 		printf("%d\n", set());
 	}
 }
+//https://algospot.com/judge/problem/read/BOARDCOVER
