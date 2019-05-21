651f61708177d712286309a4aa8ed5f1f8f23cf9
Yujin Lee <proqk@naver.com>
2019-05-21 19:22:23 +0900

    Create 콜라츠 추측.cpp

---
+++ programmers/콜라츠 추측.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    while(num != 1){
+        if(answer == 500) return -1;
+        if(num%2==0) {num/=2;}
+        else if(num%2 ==1) num = num*3 + 1;
+
+        answer++;
+    }
+    return answer;
+}
