acb584f5a5567cbd5f86ad0d3b709a3744c1071d
Yujin Lee <proqk@naver.com>
2019-07-17 03:22:32 +0900

    Create 1003 피보나치 함수.cpp

---
+++ dp/1003 피보나치 함수.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int n, a, dp0[50]={1,0},dp1[50]={0,1};
+
+int main() {
+	for(int i=2;i<=40;i++){
+		dp0[i] = dp0[i-2]+dp0[i-1];
+		dp1[i] = dp1[i-2]+dp1[i-1];
+	}
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		printf("%d %d\n", dp0[a], dp1[a]);
+	}
+}
