aade09b5195c573f7a3cfa3c4a3c49daac7b0318
Yujin Lee <proqk@naver.com>
2020-01-13 15:50:06 +0900

    Update K번째 수.py

--- programmers/K번째 수.py
+++ programmers/K번째 수.py
@@ -1,8 +1,22 @@
 def solution(array, commands):
     answer = []
-    
+
     for i in range(0,len(commands)):
-        newarr = sorted([array[k-1] for k in range(commands[i][0], commands[i][1]+1)])
-        answer.append(newarr[commands[i][2]-1])
+        newarr = sorted([array[k] for k in range(commands[i][0], commands[i][1]+1)])
+        answer.append(newarr[commands[i][2]])
 
     return answer
+
+'''
+#다른 풀이
+
+def solution(array, commands):
+    answer = []
+    for com in commands:
+        answer.append(sorted(array[com[0]-1:com[1]])[com[2]-1])
+    return answer
+
+
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+'''
