da895906420fa842aa2f509d8da119936c9031b2
Yujin Lee <proqk@naver.com>
2020-01-13 11:49:33 +0900

    Create 2523 별찍기13.py

---
+++ solve/2523 별찍기13.py
@@ -0,0 +1,12 @@
+n=int(input())
+
+for i in range(0,n):
+    for j in range(0,i+1):
+        print("*", end="")
+    print("")
+
+for i in range(0,n-1):
+    for j in range(0,n-i-1):
+        print("*", end="")
+    print("")
+
