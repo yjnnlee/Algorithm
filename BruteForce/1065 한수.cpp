530b0b1b8c957d3c4cc351588490f1129eb80ab0
Yujin Lee <proqk@naver.com>
2020-03-04 17:47:39 +0900

    Create 1065 한수.cpp

---
+++ BruteForce/1065 한수.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int n, res = 99;
+string tmp;
+int main() {
+	cin >> n;
+	if (n <= 99) res = n; //99까지는 다 한수
+	else {
+		for (int i = 100; i <= n; i++) {
+			tmp = to_string(i);
+			if (abs((tmp[2] - '0') - (tmp[1] - '0')) ==
+				abs((tmp[1] - '0') - (tmp[0] - '0'))) { //등차수열
+				if ((tmp[0] == tmp[1] && tmp[0] != tmp[2]) || //두 수만 같다면 아님 101같은 거
+					(tmp[0] == tmp[2] && tmp[0] != tmp[1]) ||
+					(tmp[1] == tmp[2] && tmp[0] != tmp[1])) {
+					continue;
+				}
+				res++;
+			}
+		}
+	}
+	cout << res;
+}
