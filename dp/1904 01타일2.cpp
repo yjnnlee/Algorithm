d2ff62a6dfd9661dd0d52e75a94616b53da4eb57
Yujin Lee <proqk@naver.com>
2020-07-28 01:52:44 +0900

    Update 1904 01타일2.cpp

--- dp/1904 01타일2.cpp
+++ dp/1904 01타일2.cpp
@@ -1,25 +1,12 @@
 #include<iostream>
-
-#define endl "\n"
-#define MODULER 15746
-#define MAX 1000000 + 1
 using namespace std;
-
-int N, int dp[MAX];
-
+int dp[1000005] = { 1,1,2,3, 5 };
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> N;
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 2;
-
-    for (int i = 3; i <= N; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
-        dp[i] = dp[i] % MODULER;
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin >> n;
+    for (int i = 5; i <= n; i++) {
+        dp[i] = (dp[i - 2] + dp[i - 1])%15746;
     }
-    cout << dp[N] << endl;
+    cout << dp[n]%15746;
 }
