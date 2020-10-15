d7ebde3ada691c23f54acefda986c43c1c2ab668
Yujin Lee <proqk@naver.com>
2020-10-15 12:57:21 +0900

    Create 15650 N과 M (2).cpp

---
+++ math/15650 N과 M (2).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> v;
+bool chk[10];
+int n, m;
+void go(int status, int cnt, int start) {
+	if (cnt == m) { //다 뽑음
+		for (auto a : v) cout << a << " ";
+		cout << "\n";
+		return;
+	}
+	for (int i = start; i < n; i++) {
+		//if (chk[i] == true) continue;
+		//chk[i] = true;
+		v.push_back(i+1);
+		go(status, cnt + 1, i + 1);
+		v.pop_back();
+		//chk[i] = false;
+	}
+}
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+	go(0, 0, 0);
+}
