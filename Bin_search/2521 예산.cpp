d8b44ea596bdfc6a11f01c0cdc59c8695414fc60
Yujin Lee <proqk@naver.com>
2020-10-17 13:42:45 +0900

    Create 2521 예산.cpp

---
+++ Bin_search/2521 예산.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+signed main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	long long n, totalsum = 0;
+	cin >> n;
+	vector<int>v(n);
+	for (int i = 0; i < n; i++) { cin >> v[i]; totalsum += v[i]; }
+	long long l = 0, r = *max_element(v.begin(), v.end()), res = 0;
+	long long totalhave;
+	cin >> totalhave;
+
+	while(l <= r) {
+		long long mid = l + r >> 1, sum = 0;
+		for (int i = 0; i < n; i++) {
+			if (mid < v[i]) sum += mid; //상한액 이상이면 상한액만
+			else sum += v[i]; //상한액 이하면 금액 전체
+		}
+		if (sum <= totalhave) { //예산보다 전체합-상한액 이상합이 크면
+			res = mid;
+			l = mid + 1; //상한액을 높게 잡아서 덜 잡는다
+		}
+		else {
+			r = mid - 1; //예산보다 작으면 상한액을 낮게 잡아서 더 잡는다
+		}
+	}
+	cout << res;
+}
